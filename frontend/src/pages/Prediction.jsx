d023fa069f2006733f56b6b3271c68b48d370d76
Srinivas palreddy <palreddysrinivas777@gmail.com>
2026-08-05 13:37:44 +0530

    Fix prediction page

--- frontend/src/pages/Prediction.jsx
+++ frontend/src/pages/Prediction.jsx
@@ -444,7 +444,6 @@ const handleChange = (e) => {
                             onChange={handleChange}
                             className="w-full border rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             required
-                            min="1"
                             max="130"
 
                         />
